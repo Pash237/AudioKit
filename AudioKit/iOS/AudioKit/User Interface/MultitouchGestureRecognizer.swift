f15400be96ce92555c728f41fdfd534b2756d271
Pavel Alexeev <pasha.alexeev@gmail.com>
2018-07-03 16:12:30 +0300

    State → MultitouchGestureRecognizerState to fix build error in Xcode 10

--- AudioKit/iOS/AudioKit/User Interface/MultitouchGestureRecognizer.swift
+++ AudioKit/iOS/AudioKit/User Interface/MultitouchGestureRecognizer.swift
@@ -85,7 +85,7 @@ open class MultitouchGestureRecognizer: UIGestureRecognizer {
     public lazy private(set) var touches = [UITouch]()
 
     /// The current gesture recognizer state, as it pertains to the `sustain` setting.
-    public enum State {
+    public enum MultitouchGestureRecognizerState {
 
         /// All touches are ended, and none are being sustained.
         case ready
@@ -99,7 +99,7 @@ open class MultitouchGestureRecognizer: UIGestureRecognizer {
     }
 
     /// The current multitouch gesture recognizer state.
-    public var multitouchState: State {
+    public var multitouchState: MultitouchGestureRecognizerState {
         if touches.isEmpty {
             return .ready
         } else if touches.filter({ $0.phase != .ended }).isNotEmpty {
